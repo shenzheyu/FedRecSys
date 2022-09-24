120fc69b7d6791161691cc80e614c2d6a3e2b74d
chaoyanghe <chaoyanghe.com@gmail.com>
2022-09-24 13:40:30 -0700

    Update standalone_main.py

--- standalone_main.py
+++ standalone_main.py
@@ -117,11 +117,10 @@ def test(model, data_loader, task_num, criterion, evaluation, device):
             categorical_fields, numerical_fields, labels = categorical_fields.to(device), numerical_fields.to(
                 device), labels.to(device)
             y = model(categorical_fields, numerical_fields)
-            y = y.to(device)
             for i in range(task_num):
                 labels_dict[i].extend(labels[:, i].tolist())
                 predicts_dict[i].extend(y[i].tolist())
-                loss_dict[i].append(criterion[i](y[i].view(-1), labels[:, i].float()))
+                loss_dict[i].append(criterion[i](y[i].view(-1).cpu(), labels[:, i].float().cpu()))
     evaluation_results, loss_results = list(), list()
     for i in range(task_num):
         evaluation_results.append(evaluation[i](labels_dict[i], predicts_dict[i]))
